5a0527dfe67683b773607a428f30758e14c8dbd4
omar080299Dg <99omar.niang@gmail.com>
2025-05-02 11:45:20 -0400

    some changes

---
+++ src/app/interfaces/Mrc.ts
@@ -0,0 +1,17 @@
+export interface Mrc{
+  code_mrc:number,
+  nom:string,
+  type:string
+}
+
+export interface Region{
+  code_region:number,
+  nom:string,
+
+}
+
+export interface UniteGeo{
+  id:number,
+  nom:string,
+
+}
--- src/app/services/projet.service.ts
+++ src/app/services/projet.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Projet } from '../interfaces/Projet';
+import { Mrc, Region, UniteGeo } from '../interfaces/Mrc';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,19 @@ export class ProjetService {
    public getAllProjet():Observable<Projet[]>{
     return this.http.get<Projet[]>(`${this.apiUrl}/projet/all`)
       }
+  public getAllMrc():Observable<Mrc[]>{
+        return this.http.get<Mrc[]>(`${this.apiUrl}/mrc/all`)
+          }
+   public getAllRegion():Observable<Region[]>{
+            return this.http.get<Region[]>(`${this.apiUrl}/region/all`)
+              }
+    public getAllUniteGO():Observable<UniteGeo[]>{
+                return this.http.get<UniteGeo[]>(`${this.apiUrl}/ugeo/all`)
+                  }
 
     public donwloadCsV(){
       return this.http.get(`${this.apiUrl}/projet/download`, { responseType: 'blob' })
     }
+
+
 }
